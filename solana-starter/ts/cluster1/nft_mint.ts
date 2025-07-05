be912d828d6bf75970cd3da57d8e30e95c238ff5
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2025-07-05 17:50:51 +0530

    completed nft-mint

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../turbin3-wallet.json"
+import wallet from "../Turbin3-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -16,11 +16,21 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    let tx = createNft(umi, {
+        mint,
+        name: "Multi Color Generative Rug #1",
+        symbol: "MCGNRUG1",
+        uri: "https://gateway.irys.xyz/3gJsftQwbmBn2271GLwJiYdAjMf5Mhw7rEz46ZcqxEwt",
+        sellerFeeBasisPoints: percentAmount(5)
+    })
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
+
+// Succesfully Minted! Check out your TX here:
+// https://explorer.solana.com/tx/4t5pceHmTzVtrqhaFYAair6kP9pZ8gt3qCZ65uf6k1So4oHbAkLHM1iVzjVRjsQ16SK2mYwB3JDzqyhoZ4UztZ78?cluster=devnet
+// Mint Address:  CvhPLk7Hr21ERryydWVXABAoR6UhiSMumGDzXAp3p4sE
